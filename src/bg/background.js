a74ae75a5289c77c7f65344f0ba21eae7f7d0a8c
Thomas White <tomw1605@users.noreply.github.com>
2025-07-29 16:51:43 +0800

    remove background page

--- src/bg/background.js
+++
@@ -1,6 +0,0 @@
-chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.type === 'getPref') {
-        var result = localStorage.getItem(message.key);
-        sendResponse(result);
-    }
-});
