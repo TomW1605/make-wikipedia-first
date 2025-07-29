47bab8195474b313d66d25792c8c01c65d101955
Thomas White <tomw1605@users.noreply.github.com>
2025-07-29 16:49:58 +0800

    convert to manifest v3
    * switch to using chrome.storage
    * add buttons for light and dark mode highlight defaults
    * add buttons to sync settings to and from chrome account

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,45 +1,29 @@
-var HIGHLIGHT_COLOR = 'rgba(255, 255, 204, 0.5)';
-var options = {};
-
-chrome.extension.sendMessage({type:'getPref', key:'domains'}, function(result) {
-    var ret = result || ['wikipedia'];
-    if (typeof ret === 'string') {
-        ret = ret.split(', ');
-    }
-    options['domains'] = ret;
-});
-
-chrome.extension.sendMessage({type:'getPref', key:'number'}, function(result) {
-    options['number'] = result || 'all';
-});
-
-chrome.extension.sendMessage({type:'getPref', key:'highlight'}, function(result) {
-    options['highlight'] = result || false;
-});
+const StorageArea = chrome.storage.local;
 
 function move_results() {
-    const results = [];
-    for (let i = 0; i < options['domains'].length; i++) {
-        const domain = options['domains'][i];
-        if (options['number'] === 'first') {
-            if (options['highlight'] === 'true') {
-                results.push($('cite:contains("' + domain + '")').parents().eq(10).css('background', HIGHLIGHT_COLOR));
-            } else {
-                results.push($('cite:contains("' + domain + '")').parents().eq(10));
-            }
-        } else if (options['number'] === 'all') {
-            $('cite:contains("' + domain + '")').each(function(i, e) {
-                if ($(e).css('visibility') === "visible")
-                {
-                    if (options['highlight'] === 'true') {
-                        $(e).parents().eq(10).css('background', HIGHLIGHT_COLOR);
-                    }
-                    results.push($(e).parents().eq(10));
+    StorageArea.get(null, function (options) {
+        const results = [];
+        for (let i = 0; i < options['domains'].length; i++) {
+            const domain = options['domains'][i];
+            if (options['number'] === 'first') {
+                if (options['highlight']) {
+                    results.push($('cite:contains("' + domain + '")').parents().eq(10).css('background', HIGHLIGHT_COLOR));
+                } else {
+                    results.push($('cite:contains("' + domain + '")').parents().eq(10));
                 }
-            });
+            } else if (options['number'] === 'all') {
+                $('cite:contains("' + domain + '")').each(function (i, e) {
+                    if ($(e).css('visibility') === "visible") {
+                        if (options['highlight']) {
+                            $(e).parents().eq(10).css('background', options['highlightColor']);
+                        }
+                        results.push($(e).parents().eq(10));
+                    }
+                });
+            }
         }
-    }
-    $('#rso').prepend(results);
+        $('#rso').prepend(results);
+    });
 }
 
 document.onreadystatechange = function() {
--- src/options/options.js
+++ src/options/options.js
@@ -1,8 +1,21 @@
+const StorageAreaSync = chrome.storage.sync;
+const StorageAreaLocal = chrome.storage.local;
+
 function save_options() {
   $('#status').hide();
-  localStorage['domains'] = $('#domains').val();
-  localStorage['number'] = $('#number').val();
-  localStorage['highlight'] = $('#highlight').is(':checked');
+
+  let domains = $('#domains').val().split(',').map(function(item) {
+    return item.trim();
+  });
+  StorageAreaLocal.set(
+    {
+      'domains': domains,
+      'number': $('#number').val(),
+      'highlight': $('#highlight').is(':checked'),
+      'highlightColor': $('#highlightColor').val()
+    }
+  );
+
   $('#status').html('Options Saved.').slideDown();
   setTimeout(function() {
     $('#status').slideUp().empty();
@@ -10,12 +23,58 @@ function save_options() {
 }
 
 function restore_options() {
-  $('#number').val(localStorage['number'] || 'all');
-  $('#domains').val(localStorage['domains'] || 'wikipedia');
-  if (localStorage['highlight'] === 'true') {
-    $('#highlight').prop('checked', true);
-  }
+  StorageAreaLocal.get(null, function (options) {
+    $('#domains').val(options['domains'].join(',') || 'wikipedia');
+    $('#number').val(options['number'] || 'all');
+    $('#highlight').prop('checked', options['highlight'] || false);
+    $('#highlightColor').val(options['highlightColor'] || '#FFFFCC');
+  });
+}
+
+function light_default() {
+  $('#highlightColor').val('#FFFFCC');
+}
+function dark_default() {
+  $('#highlightColor').val('#3D3D00');
+}
+
+function sync_to_account() {
+  save_options();
+  StorageAreaLocal.get(null, function (options) {
+    StorageAreaSync.set({
+      'domains': options['domains'] || [],
+      'number': options['number'] || 'all',
+      'highlight': options['highlight'] || false,
+      'highlightColor': options['highlightColor'] || '#FFFFCC'
+    }).then(() => {
+      $('#status').html('Options synced to account.').slideDown();
+      setTimeout(function() {
+        $('#status').slideUp().empty();
+      }, 1000);
+    });
+  });
+}
+
+function sync_from_account() {
+  StorageAreaSync.get(null, function (options) {
+    StorageAreaLocal.set({
+    'domains': options['domains'] || [],
+    'number': options['number'] || 'all',
+    'highlight': options['highlight'] || false,
+    'highlightColor': options['highlightColor'] || '#FFFFCC'
+    }).then(() => {
+    $('#status').html('Options synced from account.').slideDown();
+      setTimeout(function() {
+        $('#status').slideUp().empty();
+      }, 1000);
+    });
+  });
+    restore_options();
 }
 
 $(restore_options);
 $('#save').click(save_options);
+$('#lightDefault').click(light_default);
+$('#darkDefault').click(dark_default);
+$('#syncToAccount').click(sync_to_account);
+$('#syncFromAccount').click(sync_from_account);
